56b1afeaa67c569fdcedbfc5a286e83211c64d20
Thibault Viglino <thibault.viglino@visium.ch>
2024-12-05 20:25:35 +0100

    work

--- src/salon/chat/main.py
+++ src/salon/chat/main.py
@@ -8,9 +8,32 @@ from salon.config import config
 _ = config  # side effect env var reads for langsmith
 
 
-def get_response(text, history):
-    return asyncio.run(chatbot.get_response(text))
+def get_response(text):
+    response = asyncio.run(chatbot.get_response(text))
+    print(response)
+    return None
 
 
-demo = gr.ChatInterface(fn=get_response, type="messages")
-demo.launch()
+TITLE = "Super Invention 3000™"
+theme = gr.Theme.from_hub("YTheme/Minecraft")
+css = "footer {visibility: hidden}"
+
+with gr.Blocks(title=TITLE, theme=theme, css=css) as demo:
+
+    gr.Markdown(f"# {TITLE}")
+
+    textbox = gr.Textbox(
+        label="Quelle ambiance voulez vous vivre ?",
+        placeholder="Décrivez ici une expérience, un sentiment, un rêve…",
+        lines=5,
+    )
+    btn = gr.Button("C’est parti !", variant="primary")
+
+    btn.click(fn=lambda: gr.update(interactive=False), inputs=None, outputs=btn).then(
+        fn=get_response, inputs=textbox, outputs=textbox
+    ).then(fn=lambda: gr.update(interactive=True), inputs=None, outputs=btn)
+
+
+demo.launch(
+    show_api=False,
+)
